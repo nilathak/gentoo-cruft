91eb1482c56b251aa22655cacfabc39f6147f5ab
Hannes Schweizer <nilathak@users.noreply.github.com>
2023-09-10 01:24:12 +0200

    pylon module-level changes:
    - general cleanup
    - add async version of the log_exec_time decorator
    - add path_lock context manager (needed just for backup runs now)

--- pylon/__init__.py
+++ pylon/__init__.py
@@ -10,24 +10,60 @@ Etymology: Greek pylOn, from pylE gate
 Refer to module docstrings for detailed explanations.
 '''
 
+import asyncio
 import datetime
+import fcntl
 import functools
+import inspect
 import itertools
 import math
+import os
+
+# =====================================================================================================================
+# context managers
+# =====================================================================================================================
+class path_lock():
+    def __init__(self, _path):
+        self.__dict__.update(locals())
+        assert(os.path.exists(_path))
+
+    def __enter__(self):
+        self._fd = os.open(self._path, os.O_RDONLY)
+        try:
+            fcntl.flock(self._fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except OSError:
+            raise Exception(f'path is locked by another process: {self._path}') from None
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        fcntl.flock(self._fd, fcntl.LOCK_UN)
+        os.close(self._fd)
 
 # =====================================================================================================================
 # decorators
 # =====================================================================================================================
 def log_exec_time(func):
     'method decorator to explicitly log execution time'
+
     # keep original attributes for decorated function (eg, __name__, __doc__, ...)
     @functools.wraps(func)
-    def __wrapper(self, *args, **kwargs):
+    def method_wrapper(self, *args, **kwargs):
         t1 = datetime.datetime.now()
         ret = func(self, *args, **kwargs)
+        # FIXME remove old style pylon log function after complete migration
         self.ui.info(func.__name__ + ' took ' + str(datetime.datetime.now() - t1) + ' to complete...')
         return ret
-    return __wrapper
+
+    @functools.wraps(func)
+    async def async_method_wrapper(self, *args, **kwargs):
+        t1 = datetime.datetime.now()
+        ret = await func(self, *args, **kwargs)
+        self.logger.info(func.__name__ + ' took ' + str(datetime.datetime.now() - t1) + ' to complete...')
+        return ret
+
+    if inspect.iscoroutinefunction(func):
+        return async_method_wrapper
+    return method_wrapper
 
 # =====================================================================================================================
 # exceptions
@@ -42,9 +78,9 @@ class script_error(Exception):
     def owner(self):
         return self._owner
 
-    def __init__(self, msg='No error info available', owner=None):
-        self._msg = msg
-        self._owner = owner
+    def __init__(self, _msg='No error info available', _owner=None):
+        super().__init__()
+        self.__dict__.update(locals())
 
     def __str__(self):
         return self.msg
@@ -60,14 +96,14 @@ def chunk(n, iterable):
             return
         yield chunk
 	
-def flatten(l):
+def flatten(iterable):
     'flatten multidimensional lists'
-    for el in l:
-        if hasattr(el, '__iter__') and not isinstance(el, str):
-            for sub in flatten(el):
-                yield sub
+    for elem in iterable:
+        if hasattr(elem, '__iter__') and not isinstance(elem, str):
+            for sub_elem in flatten(elem):
+                yield sub_elem
         else:
-            yield el
+            yield elem
 
 def unique_logspace(data_points, interval_range):
     'provide logarithmically spaced integers in a certain range'
@@ -75,11 +111,7 @@ def unique_logspace(data_points, interval_range):
     exp = [x * math.log(interval_range)/data_points for x in range(0, data_points)]
     logspace = [int(round(math.exp(x))) for x in exp]
     for idx,val in enumerate(logspace):
-        if idx > 0:
-            if val <= new_val:
-                new_val = new_val + 1
-            else:
-                new_val = val
+        if val <= (idx + 1):
+            yield idx + 1
         else:
-            new_val = val
-        yield new_val
+            yield val
